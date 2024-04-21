120cc9b6aa6b8e01aff02d6932ce973c429a7c2e
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-04-20 21:59:38 -0700

    Done!

--- src/M4Shield/EventListener.php
+++ src/M4Shield/EventListener.php
@@ -34,41 +34,45 @@ class EventListener implements Listener {
         $player->getInventory()->setItemInHand(VanillaItems::AIR());
     }
 
-    private function processBlockedWords(string $message): string {
-        $blockedWords = $this->getConfig()->getNested("chatblocker.blockedWords", []);
-        $replacementChar = $this->getConfig()->getNested("chatblocker.replacementChar", "*");
+    //Temp removal.
+    //PHPStan flags these as unused :/
+    //Message by Terpz710:)
+    
+    //private function processBlockedWords(string $message): string {
+        //$blockedWords = $this->getConfig()->getNested("chatblocker.blockedWords", []);
+        //$replacementChar = $this->getConfig()->getNested("chatblocker.replacementChar", "*");
 
-        $blockedWordsSet = array_flip($blockedWords);
-        $pattern = '/\b(' . implode('|', array_map('preg_quote', $blockedWords)) . ')\b/i';
-        $message = preg_replace_callback($pattern, function($matches) use ($replacementChar) {
-            return str_repeat($replacementChar, mb_strlen($matches[0]));
-        }, $message);
+        //$blockedWordsSet = array_flip($blockedWords);
+        //$pattern = '/\b(' . implode('|', array_map('preg_quote', $blockedWords)) . ')\b/i';
+        //$message = preg_replace_callback($pattern, function($matches) use ($replacementChar) {
+            //return str_repeat($replacementChar, mb_strlen($matches[0]));
+        //}, $message);
 
-        return $message;
-    }
+        //return $message;
+    //}
 
-    private function processLeak(string $message, string $replacementChar): string {
-        $ipv4Pattern = '/\b(?:\d{1,3}(?:\s|\.|,)){3}\d{1,3}\b/';
-        $ipv6Pattern = '/(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}/';
+    //private function processLeak(string $message, string $replacementChar): string {
+        //$ipv4Pattern = '/\b(?:\d{1,3}(?:\s|\.|,)){3}\d{1,3}\b/';
+        //$ipv6Pattern = '/(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}/';
 
-        if (preg_match($ipv4Pattern, $message) || preg_match($ipv6Pattern, $message)) {
-            $message = preg_replace($ipv4Pattern, str_repeat($replacementChar, 7), $message);
-            $message = preg_replace($ipv6Pattern, str_repeat($replacementChar, 7), $message);
-        }
+        //if (preg_match($ipv4Pattern, $message) || preg_match($ipv6Pattern, $message)) {
+            //$message = preg_replace($ipv4Pattern, str_repeat($replacementChar, 7), $message);
+            //$message = preg_replace($ipv6Pattern, str_repeat($replacementChar, 7), $message);
+        //}
 
-        return $message;
-    }
+        //return $message;
+    //}
 
     public function onLogin(PlayerLoginEvent $event): void {
         $player = $event->getPlayer();
-        $ip = $player->getNetworkSession()->getAddress();
+        $ip = $player->getNetworkSession()->getIp();
         $users = 0;
         $antibotEnabled = $this->getConfig()->getNested("antibot.enabled", true);
         $antibotMaxCount = $this->getConfig()->getNested("antibot.maxCount", 4);
 
         if ($antibotEnabled) {
             foreach ($this->getServer()->getOnlinePlayers() as $onlinePlayer) {
-                if ($ip == $onlinePlayer->getNetworkSession()->getAddress()) {
+                if ($ip == $onlinePlayer->getNetworkSession()->getIp()) {
                     $users++;
                 }
             }
