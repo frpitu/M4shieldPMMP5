8f27d566adf0e785c29b69f03d88897eca96123a
Luiz Guilherme S. Munhoz ✨ <94804469+frpitu@users.noreply.github.com>
2024-04-28 17:56:51 -0300

    Add files via upload

--- src/M4Shield/EventListener.php
+++ src/M4Shield/EventListener.php
@@ -1,9 +1,20 @@
 <?php
 
+/**
+* 
+* ███╗░░░███╗░░██╗██╗████████╗██╗░░██╗███████╗██╗░░░██╗░██████╗
+* ████╗░████║░██╔╝██║╚══██╔══╝██║░░██║██╔════╝██║░░░██║██╔════╝
+* ██╔████╔██║██╔╝░██║░░░██║░░░███████║█████╗░░██║░░░██║╚█████╗░
+* ██║╚██╔╝██║███████║░░░██║░░░██╔══██║██╔══╝░░██║░░░██║░╚═══██╗
+* ██║░╚═╝░██║╚════██║░░░██║░░░██║░░██║███████╗╚██████╔╝██████╔╝
+* ╚═╝░░░░░╚═╝░░░░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚══════╝░╚═════╝░╚═════╝░
+*
+*/
+
 namespace M4Shield;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\event\player\PlayerPreLoginEvent;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\event\player\PlayerCommandPreprocessEvent;
@@ -12,82 +23,229 @@ use pocketmine\item\Item;
 use pocketmine\Player;
 
 use M4Shield\Main;
+use M4Shield\Util\Cache;
 
-class EventListener implements Listener {
+class EventListener implements Listener
+{
+    private Main $main;    
+    private array $configCache;
+    private array $antiSpam;
+    
+    private const IPV4_REGEX = '/\b(?:\d{1,3}\.){3}\d{1,3}\b/';
+    private const IPV6_REGEX = '/(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}/';
 
-    private Main $main;
-
-    public function __construct(Main $main) {
+    public function __construct(Main $main)
+    {
         $this->main = $main;
     }
 
-    private function getServer(): \pocketmine\Server {
+    public function getServer(): Server
+    {
         return $this->main->getServer();
     }
-
-    private function getConfig(): \pocketmine\utils\Config {
-        return $this->main->getConfig();
-    }
-
-    private function removeBlockedItem(Player $player): void {
-        $player->getInventory()->setItemInHand(Item::get(0, 0));
-    }
-
-    private function processBlockedWords(string $message): string {
-        $blockedWords = $this->getConfig()->getNested("chatblocker.blockedWords", []);
-        $replacementChar = $this->getConfig()->getNested("chatblocker.replacementChar", "*");
-
-        $blockedWordsSet = array_flip($blockedWords);
-        $pattern = '/\b(' . implode('|', array_map('preg_quote', $blockedWords)) . ')\b/i';
-        $message = preg_replace_callback($pattern, function($matches) use ($replacementChar) {
-            return str_repeat($replacementChar, mb_strlen($matches[0]));
-        }, $message);
-
-        return $message;
-    }
-
-    private function processLeak(string $message, string $replacementChar): string {
-        $ipv4Pattern = '/\b(?:\d{1,3}(?:\s|\.|,)){3}\d{1,3}\b/';
-        $ipv6Pattern = '/(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}/';
-
-        if (preg_match($ipv4Pattern, $message) || preg_match($ipv6Pattern, $message)) {
-            $message = preg_replace($ipv4Pattern, str_repeat($replacementChar, 7), $message);
-            $message = preg_replace($ipv6Pattern, str_repeat($replacementChar, 7), $message);
+    
+    private function getConfigValue(string $key, mixed $default = null): mixed
+    {
+        if (Cache::hasCache($key)) {
+            return Cache::get($key);
         }
 
-        return $message;
+        $value = $this->main->getConfig()->getNested($key, $default);
+        Cache::add($key, $value);
+
+        return $value;
     }
 
-    public function onLogin(PlayerLoginEvent $event): void {
-        $player = $event->getPlayer();
-        $ip = $player->getAddress();
-        $users = 0;
-        $antibotEnabled = $this->getConfig()->getNested("antibot.enabled", true);
-        $antibotMaxCount = $this->getConfig()->getNested("antibot.maxCount", 4);
+    private function processBlockedWords(string $msg): string
+    {
+        $blockedWords = $this->getConfigValue("chatblocker.blockedWords", []);
+        $replacementChar = $this->getConfigValue("chatblocker.replacementChar", "*");
 
-        if ($antibotEnabled) {
-            foreach ($this->getServer()->getOnlinePlayers() as $onlinePlayer) {
-                if ($ip == $onlinePlayer->getAddress()) {
-                    $users++;
+        foreach ($blockedWords as $word)
+        {
+            if (stripos($msg, $word) !== false)
+            {
+                $msg = str_replace($word, str_repeat($replacementChar, strlen($word)), $msg);
+            }
+        }
+
+        return $msg;
+    }
+    
+    private function hasBlockedWordsInName(string $name): bool
+    {
+        $blockedWords = $this->getConfigValue("nameblocker.blockedWords", []);
+
+        foreach ($blockedWords as $word) {
+            if (stripos($name, $word) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function hasIpAddress(string $msg): bool
+    {
+        return preg_match(self::IPV4_REGEX, $msg) || preg_match(self::IPV6_REGEX, $msg);
+    }
+
+    private function blockIpLeak(string $msg): string
+    {
+        $msg = preg_replace(self::IPV4_REGEX, str_repeat($this->getConfigValue("ipleakblock.replacementChar", "*"), 7), $msg);
+        $msg = preg_replace(self::IPV6_REGEX, str_repeat($this->getConfigValue("ipleakblock.replacementChar", "*"), 7), $msg);
+
+        return $msg;
+    }
+
+    public function onPreLogin(PlayerPreLoginEvent $e): void
+    {
+        $p = $e->getPlayer();
+        $ip = $p->getAddress();
+        
+        if (!$e->isCancelled())
+        {
+            $users = 0;
+            $antibotEnabled = $this->getConfigValue("antibot.enabled", true);
+            $nameBlockerEnabled = $this->getConfigValue("nameblocker.enabled", true);
+            $antibotmaxCount = $this->getConfigValue("antibot.maxCount", 4);
+
+            if ($antibotEnabled) {
+                foreach ($this->getServer()->getOnlinePlayers() as $ps)
+                {
+                    if ($ip == $ps->getAddress())
+                    {
+                        $users++;
+                    }
+                }
+            
+                if ($users >= $antibotmaxCount)
+                {
+                    $this->getServer()->getNetwork()->blockAddress($ip, -1);
                 }
             }
-            if ($users >= $antibotMaxCount) {
-                $this->getServer()->getNetwork()->blockAddress($ip, -1);
+        
+            if ($nameBlockerEnabled && $this->hasBlockedWordsInName($p->getName()))
+            {
+                $e->setKickMessage($this->getConfigValue("nameblocker.message", "§8[§bM4Shield§8] §cExiste uma palavra bloqueada no seu nick/nome"));
+                $e->setCancelled(true);
             }
         }
     }
 
-    public function onUse(PlayerInteractEvent $event): void {
-        $player = $event->getPlayer();
-        $item = $event->getItem();
-        $id = $item->getId();
-        $antigriefEnabled = $this->getConfig()->getNested("antigrief.enabled", true);
-        $blockedItems = $this->getConfig()->getNested("antigrief.blockedItems", []);
+    public function onUse(PlayerInteractEvent $e): void
+    {
+        $p = $e->getPlayer();
+        $item = $e->getItem();
+        
+        if (!$e->isCancelled())
+        {
+            $id = $item->getId();
+            $antigriefEnabled = $this->getConfigValue("antigrief.enabled", true);
+            $blockedItems = $this->getConfigValue("antigrief.blockedItems", []);
 
-        if ($antigriefEnabled && in_array($id, $blockedItems)) {
-            $this->removeBlockedItem($player);
-            $player->sendMessage($this->getConfig()->getNested("antigrief.message", "§8[§bM4Shield§8] §cEste item está bloqueado."));
-            $event->setCancelled(true);
+            if ($antigriefEnabled && in_array($id, $blockedItems))
+            {
+                $p->getInventory()->setItemInHand(Item::get(0, 0));
+                if ($p->getInventory()->contains(Item::get(327, 0, 1)))
+                {
+                    $p->getInventory()->removeItem(Item::get(327, 0, 1));
+                }
+            
+                $p->sendMessage($this->getConfigValue("antigrief.message", "§8[§bM4Shield§8] §cEste item está bloqueado"));
+                $e->setCancelled(true);
+            }
+        }
+    }
+
+    public function onHeld(PlayerItemHeldEvent $e) : void
+    {
+        $p = $e->getPlayer();
+        $item = $e->getItem();
+        
+        if (!$e->isCancelled())
+        {
+            $id = $item->getId();
+            $antigriefEnabled = $this->getConfigValue("antigrief.enabled", true);
+            $antigriefItemInHand = $this->getConfigValue("antigrief.itemInHand", false);
+            $blockedItems = $this->getConfigValue("antigrief.blockedItems", []);
+
+            if ($antigriefEnabled && in_array($id, $blockedItems))
+            {
+                if ($antigriefItemInHand)
+                {
+                    $p->getInventory()->setItemInHand(Item::get(0, 0));
+                    if ($p->getInventory()->contains(Item::get(327, 0, 1)))
+                    {
+                        $p->getInventory()->removeItem(Item::get(327, 0, 1));
+                    }
+                }
+                $p->sendMessage($this->getConfigValue("antigrief.message", "§8[§bM4Shield§8] §cEste item está bloqueado"));
+                $e->setCancelled(true);
+            }
+        }
+    }
+
+    public function onChat(PlayerChatEvent $e) : void
+    {
+        $p = $e->getPlayer();
+        $msg = $e->getMessage();
+
+        if (!$e->isCancelled())
+        {
+            $chatBlockerEnabled = $this->getConfigValue("chatblocker.enabled", false);
+            $ipLeakBlockEnabled = $this->getConfigValue("ipleakblock.enabled", true);
+            $antiSpamEnabled = $this->getConfigValue("antispam.enabled", true);
+            
+            if ($antiSpamEnabled)
+            {
+                if(isset($this->antiSpam[$p->getName()]))
+                {
+                    $time = time() - $this->antiSpam[$p->getName()];
+                    $antiSpamTime = $this->getConfigValue("antispam.time", 2);
+                    if($time <= $antiSpamTime)
+                    {
+                        $e->setCancelled(true);
+                        $p->sendMessage(str_replace('{$seg}', $antiSpamTime, $this->getConfigValue("antispam.message", '§8[§bM4Shield§8] §cAguarde {$seg} segundos para enviar outra mensagem')));
+                    }
+                }
+                $this->antiSpam[$p->getName()] = time();
+            }
+        
+            if ($chatBlockerEnabled)
+            {
+                $e->setMessage($this->processBlockedWords($msg));
+            }
+        
+            if ($ipLeakBlockEnabled && $this->hasIpAddress($msg))
+            {
+                $e->setMessage($this->blockIpLeak($msg));
+            }
+            $ccmd = explode(" ", $msg);
+            $cmd = strtolower(substr(array_shift($ccmd), 1));
+            $commandBlockerEnabled = $this->getConfigValue("commandblocker.enabled", true);
+            $blockedCommands = $this->getConfigValue("commandblocker.blockedCommands", []);
+            $allowedPlayers = $this->getConfigValue("commandblocker.allowedPlayers", []);
+            $chatBlockerEnabled = $this->getConfigValue("chatblocker.enabled", false);
+            $blockInCommands = $this->getConfigValue("chatblocker.blockInCommands", false);
+            $ipLeakBlockEnabled = $this->getConfigValue("ipleakblock.enabled", true);
+            $ipBlockInCommands = $this->getConfigValue("ipleakblock.blockInCommands", true);
+        
+            if ($commandBlockerEnabled && in_array($cmd, $blockedCommands) && !in_array(strtolower($p->getName()), $allowedPlayers))
+            {
+                $p->sendMessage($this->getConfigValue("commandblocker.message", "§8[§bM4Shield§8] §cEste comando está bloqueado"));
+                $e->setCancelled(true);
+            }
+        
+            if ($blockInCommands && $chatBlockerEnabled)
+            {
+                $e->setMessage($this->processBlockedWords($msg));
+            }
+        
+            if ($ipLeakBlockEnabled && $this->hasIpAddress($msg) && $ipBlockInCommands)
+            {
+                $e->setMessage($this->blockIpLeak($msg, $this->getConfigValue("ipleakblock.replacementChar", "*")));
+            }
         }
     }
 }
